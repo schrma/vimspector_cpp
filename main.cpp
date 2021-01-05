48553d9231c126539d4fd399a1f24ef22bcd9cf2
Marco Leica <marco.schroeder@leica-geosystems.com>
2021-01-05 12:08:22 +0100

    Add parameter

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+using namespace std;
 
-int main()
+int main( int argc, char *argv[], char *envp[] )
 {
-    std::cout << "Hello World!\n";
-    int index = 0;
+  int count;
+  cout << "\nCommand-line arguments:\n";
+  for( count = 0; count < argc; count++ )
+    cout << "  argv[" << count << "]   "
+      << argv[count] << "\n";
+  cout << "Hello World!\n";
+  int index = 0;
     int a = 0;
     for(int i = 0; i<10; i++)
     {
       index = i;
       a = a + i;
     }
-    std::cout << "index=" << index << "\n";
-    std::cout << "a=" << a << "\n";
+    cout << "index=" << index << "\n";
+    cout << "a=" << a << "\n";
     return 0;
 }
